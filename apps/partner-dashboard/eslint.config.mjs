b31b87456987174f3dc3a60a5652e4e7651d410d
Shriyash Sawant <sawant.shriyash08@gmail.com>
2026-06-14 12:44:36 +0530

    feat: add backend boundary guardrails system

---
+++ apps/partner-dashboard/eslint.config.mjs
@@ -0,0 +1,21 @@
+import nextConfig from "eslint-config-next";
+
+export default [
+  ...nextConfig,
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/immutability": "off",
+      "react-hooks/purity": "off",
+      "react-hooks/preserve-manual-memoization": "off",
+      "react-hooks/static-components": "off",
+      "react-hooks/refs": "off",
+      "import/no-anonymous-default-export": "off",
+      "jsx-a11y/alt-text": "off",
+    },
+  },
+];
