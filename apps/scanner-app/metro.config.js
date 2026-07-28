a63552de4131a5eb66ae1ff6d8da488f1a94aa8c
keshvi1209 <keshviagarwal2004@gmail.com>
2026-07-28 12:31:04 +0530

    chore: fix scanner app metro file watching conflict

--- apps/scanner-app/metro.config.js
+++ apps/scanner-app/metro.config.js
@@ -4,4 +4,18 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Exclude Next.js and build folders of other workspaces from Metro to prevent watcher crashes
+if (!config.resolver.blockList) {
+  config.resolver.blockList = [];
+}
+config.resolver.blockList.push(
+  /[\\/]apps[\\/]partner-dashboard[\\/]\.next[\\/]/,
+  /[\\/]apps[\\/]guest-portal[\\/]\.next[\\/]/,
+  /[\\/]apps[\\/]admin-console[\\/]\.next[\\/]/,
+  /[\\/]apps[\\/]partner-dashboard[\\/]dist[\\/]/,
+  /[\\/]apps[\\/]guest-portal[\\/]dist[\\/]/,
+  /[\\/]apps[\\/]admin-console[\\/]dist[\\/]/,
+  /[\\/]\.turbo[\\/]/,
+);
+
 module.exports = withNativeWind(config, { input: './global.css' });
